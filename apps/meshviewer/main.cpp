646d5d8b1baacb2be380b5c4ebdaa025de8a9620
Eric Lee <levanhong05@gmail.com>
2017-04-25 17:16:56 +0700

    Update high resolution

--- apps/meshviewer/main.cpp
+++ apps/meshviewer/main.cpp
@@ -11,10 +11,18 @@
 ********************************************************************************/
 
 #include "meshviewer.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
+    //qputenv("QT_SCALE_FACTOR", "1");
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
     QApplication a(argc, argv);
     MeshViewer meshMagic3D;
     meshMagic3D.show();
